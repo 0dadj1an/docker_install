e98fc6d6bf1ee07149b07a164cfb3ce4f9ffbe4a
0dadj1an <0dadj1an@users.noreply.github.com>
2018-09-04 23:29:39 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,28 +1,4 @@
-### old
 
-#!/bin/bash
-
-# compiled from https://docs.docker.com/engine/installation/linux/debian/#/debian-jessie-80-64-bit
-
-sudo apt-get update
-sudo apt-get dist-upgrade -y
-sudo apt-get install apt-transport-https ca-certificates -y
-
-sudo sh -c "echo deb https://apt.dockerproject.org/repo debian-jessie main > /etc/apt/sources.list.d/docker.list"
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-
-sudo apt-get update
-sudo apt-cache policy docker-engine
-sudo apt-get install docker-engine -y --allow-unauthenticated
-
-
-sudo service docker start
-sudo docker run hello-world
-
-sudo group add docker
-sudo groupadd docker
-sudo gpasswd -a $USER docker
-sudo service docker restart
 
 
 ## new
@@ -38,3 +14,11 @@ sudo systemctl status docker
 docker -v
 sudo usermod -aG docker $USER # If you want to run Docker commands as a non-root user without prepending sudo you’ll need to add your user to the docker group which is created during the installation of the Docker CE package. You can do that by typing
 docker container run hello-world
+
+
+# jenkins how to
+
+docker pull jenkins jenkins/jenkins:lts
+docker run -d -v /var/jenkins_home:/var/jenkins_home:z -p 8080:8080 -p 50000:50000 --name myjenkins jenkins/jenkins:lts
+docker update --restart=always 0576df221c0b
+
